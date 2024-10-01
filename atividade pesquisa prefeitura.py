2a6176da2d28e57c6b06d3d757d02c969868789c
llaradearaujo <laradearaujo300@gmail.com>
2024-10-01 19:34:44 +0000

    atividade prefeitura

--- atividade pesquisa prefeitura.py
+++ atividade pesquisa prefeitura.py
@@ -9,14 +9,14 @@ familias = []
 salario = []
 filhos = []
 
-print(""" 
+while True:
+    
+    print(""" 
 =====MENU====
 1 - adicionar familia
 2 - exibir resultados
 3 - sair
 """)
-
-while True:
     opcao = int(input("digite a opção desejada: "))
     if opcao == 1:
         numero_filhos = int(input("digite o numero de filhos: "))
@@ -25,40 +25,35 @@ while True:
         salario.append(salario_familia)
         familias.append(1)
         os.system("cls || clear")
-        print(""" 
-        =====MENU====
-        1 - adicionar familia
-        2 - exibir resultados
-        3 - sair
-        """)        
+
+        total_familias = sum(familias)
+        soma_salarios = sum(salario)
+        soma_filhos = sum(filhos)
+        maior_salario = max(salario)
+        menor_salario = min(salario)
+
+        media_filhos = soma_filhos / total_familias
+        media_salarios = soma_salarios / total_familias
+              
     elif opcao == 2:
-        break
+
+
+        print(f"total de familias: {total_familias}")
+        print(f"média de salário: {media_salarios}")
+        print(f"média de filhos: {media_filhos}")
+        print(f"maior salário: {maior_salario}")
+        print(f"menor salário: {menor_salario}")
+        
     elif opcao == 3:
         print("saindo do programa")
         break
     else:
         print("opção inválida")
 
-total_familias = sum(familias)
-soma_salarios = sum(salario)
-soma_filhos = sum(filhos)
-maior_salario = max(salario)
-menor_salario = min(salario)
-
-media_filhos = soma_filhos / total_familias
-media_salarios = soma_salarios / total_familias
-
-if opcao != 3:
-    print(f"total de familias: {total_familias}")
-    print(f"média de salário: {media_salarios}")
-    print(f"média de filhos: {media_filhos}")
-    print(f"maior salário: {maior_salario}")
-    print(f"menor salário: {menor_salario}")
-
 nome_do_arquivo = "pesquisa_prefeitura.txt"
 
-with open(nome_do_arquivo, "a") as arquivo_prefeitura:
-    arquivo_prefeitura.write(f"\n total familias: {total_familias}")
+with open(nome_do_arquivo, "w") as arquivo_prefeitura:
+    arquivo_prefeitura.write(f"\n total familias:{total_familias}")
     arquivo_prefeitura.write(f"\n media de salarios: {media_salarios}")
     arquivo_prefeitura.write(f"\n media de filhos: {media_filhos}")
     arquivo_prefeitura.write(f"\n maior salario: {maior_salario}")
@@ -68,5 +63,17 @@ with open(nome_do_arquivo, "a") as arquivo_prefeitura:
 arquivo_prefeitura.close()
 print("=====DADOS SALVOS COM SUCESSO====")
 
+with open(nome_do_arquivo,"r")as arquivo_de_origem:
+    for linha in arquivo_de_origem:
+       linha.strip().split("\n")
+    
+arquivo_prefeitura.close()
+
+print("=====EXIBINDO DADOS======")
+print(f"total familias:{total_familias}")
+print(f"media de salarios: {media_salarios}")
+print(f"media de filhos: {media_filhos}")
+print(f"maior salario: {maior_salario}")
+print(f"menor salario: {menor_salario}")
 
 
